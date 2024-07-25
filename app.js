433672ecb5a2671605ff2eea3f09dd0f7fdca2a4
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-07-25 15:26:36 +0200

    aaa

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+app.get('/api/skill-gems', async (req, res) => {
+    try {
+        const response = await axios.get('https://poe.ninja/api/data/itemoverview?league=Necropolis&type=SkillGem');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching data');
+    }
+});
+//root should return a html file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
