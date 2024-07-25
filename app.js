2ab27e7ea966511dea22600ecc758358d1afd34b
mantarias <mantas2630@hotmail.com>
2024-07-25 22:39:22 +0200

    added radio boxes for quality and level

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3030;
+app.use(express.static("public"));
+app.get("/api/skill-gems", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://poe.ninja/api/data/itemoverview?league=Necropolis&type=SkillGem",
+    );
 
-app.get('/api/skill-gems', async (req, res) => {
-    try {
-        const response = await axios.get('https://poe.ninja/api/data/itemoverview?league=Necropolis&type=SkillGem');
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send('Error fetching data');
-    }
+    res.json(response.data);
+  } catch (error) {
+    console.log(error)
+    res.status(500).send("Error fetching data");
+  }
 });
 //root should return a html file
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://127.0.0.1:${port}`);
+  console.log(`Server running at https://127.0.0.1:${port}`);
 });
