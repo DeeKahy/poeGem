5fe2195961c1b756e4dbd967ed3164a614da4ebd
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-07-25 15:57:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 4002;
+const port = 3030;
 
 app.get('/api/skill-gems', async (req, res) => {
     try {
